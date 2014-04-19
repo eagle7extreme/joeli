af38c5be52c76620848270a7822ad20f64546cc6
Jorge Gonzalez <jgonzalez@localhost.localdomain>
2014-04-18 22:46:20 -0400

    Refactor #2

--- joeli-pdf-manager/src/main/java/com/joeli/rest/RestApplication.java
+++
@@ -1,9 +0,0 @@
-package com.joeli.rest;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/service")
-public class RestApplication extends Application {
-
-}
